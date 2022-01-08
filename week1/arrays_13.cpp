05184e87b57f8e8ec504fe4d38ab9388a1e0005c
Kiran Srinivasan <kiransrini9@gmail.com>
2022-01-08 10:42:26 +0530

    test

--- week1/arrays_13.cpp
+++ week1/arrays_13.cpp
@@ -1,3 +1,5 @@
+// Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
